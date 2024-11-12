8eb35f0b4138e5f28c385d7637b82c84c4ddd98c
FFusa1 <khachidzedimitri@gmail.com>
2024-11-12 13:37:17 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
 
 // List of products with ID, title, price, and image URL
-window.addEventListener('load', function() {
-    console.log("Page is fully loaded.");
-});
-
-
-
-
-
 const products = [
     { id: 1, title: "Leather Coat", price: 149.99, Image: "https://static.zara.net/assets/public/fded/e3b5/8afa45f9b357/533e37622b3c/04341728814-015-p/04341728814-015-p.jpg?ts=1726156162824&w=750" },
     { id: 2, title: "Padded overshirt", price: 124.99, Image: "https://static.zara.net/assets/public/64b0/07fd/6f244d6b8f29/438cd7167418/01063383500-p/01063383500-p.jpg?ts=1730634458999&w=750" },
@@ -18,8 +10,6 @@ const products = [
     { id: 7, title: "Valvet Overshirt", price: 114.99, Image: "https://static.zara.net/assets/public/68a7/b6bd/7237419daf1e/feeb10739e26/05320422800-p/05320422800-p.jpg?ts=1730642380735&w=750" },
     { id: 8, title: "Trousers With Double Pleat", price: 129.99, Image: "https://static.zara.net/assets/public/ce5e/60a7/b36e442a9211/4b3b7f3f8804/01255556753-015-p/01255556753-015-p.jpg?ts=1730387012662&w=750" }
 ];
-window.addEventListener('load', function() {
-    console.log("Page is fully loaded.");
 
 // Elements in HTML where products, cart items, and cart total are displayed
 const productList = document.getElementById('productList');
@@ -153,7 +143,6 @@ if (window.location.pathname.includes('cart.html')) {
     renderProducts();
 }
 
-});
 
 // Navbar and menu icon toggle functionality
 const menuIcon = document.getElementById("menu-icon");
